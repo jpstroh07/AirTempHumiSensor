13dc4b8e8b641a5bad0bec8d0010ca4e1c4db4b6
Jan Pinto Strohhäusl <93579089+jpstroh07@users.noreply.github.com>
2026-01-10 11:42:25 +0100

    update offline readings

--- AirTempHumiSensor/src/main.cpp
+++ AirTempHumiSensor/src/main.cpp
@@ -4,6 +4,18 @@
 #include <THWifi.h>
 #include <THServer.h>
 
+int offlineTick = 0;
+
+void readAndUpdate()
+{
+	float temperature = tempReading();
+	float humidity = humiReading();
+
+	updatePageContent(temperature, humidity);
+
+	offlineTick = 0;
+}
+
 void setup()
 {
 	Serial.begin(9600);
@@ -15,15 +27,18 @@ void loop()
 {
 	if (isInRange())
 	{
-		float temperature = tempReading();
-		float humidity = humiReading();
-
-		updatePageContent(temperature, humidity);
+		readAndUpdate();
 	}
 	else
 	{
 		Serial.println("Object out of range.");
 		sensorOffline();
+
+		offlineTick++;
+
+		if (offlineTick >= 5) {
+			readAndUpdate();
+		}
 	}
 
 	delay(2000);
